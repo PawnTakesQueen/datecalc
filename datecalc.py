dff11e240bce935ad9f28300384462ea00834931
Vi <vi@pariahvi.com>
2014-06-12 05:50:15 -0500

    Update datecalc.py

--- datecalc.py
+++ datecalc.py
@@ -1,15 +1,26 @@
-__version__ = '2.0.3'
-__author__ = 'Violet (PariahVi)'
+# Copyright 2012-2014 PariahVi (http://pariahvi.com).
+# LatchBox is licensed under a BSD License.
+# Read LICENSE.txt for more license text.
 
-month_offset = ([4, 3], [0, 6], [0], [3], [5], [1], [3], [6], [2], [4], [0],
-                [2])
-days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-        'Saturday')
-types = ('GREGORIAN', 'CE', 'JULIAN', 'ENGLISH', 'ROMAN')
-month_names = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
-               'August', 'September', 'October', 'November', 'December')
+# A Module to Calculate the Day of the Week of Any Date
+
+__version__ = '1.0.3.0'
+__author__ = 'PariahVi (http://pariahvi.com)'
 
 
+MONTH_OFFSET = [
+    [4, 3], [0, 6], [0], [3], [5], [1], [3], [6], [2], [4], [0], [2]
+]
+DAYS = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+    'Saturday'
+]
+CAL_TYPES = [
+    'GREGORIAN', 'CE', 'JULIAN', 'ENGLISH', 'ROMAN'
+]
+
+
+# Figure out if leap year for CE style dates.
 def ce_leap_year(y):
     if y % 100 == 0:
         if y % 400 == 0:
@@ -19,36 +30,39 @@ def ce_leap_year(y):
     return 0
 
 
-def jul_leap_year(y, ny):
+# Figure out if leap year for Julian style dates.
+def jul_leap_year(y):
     if y < 0:
-        ny = (y + 1) % 700
-    if ny % 4 == 0:
+        y = (y + 1) % 700
+    if y % 4 == 0:
         return 1
     return 0
 
 
-def is_leap_year(year, type):
-    if type == 'GREGORIAN':
+# Figure out if year is a leap year for cal_type.
+def is_leap_year(year, cal_type):
+    if cal_type == 'GREGORIAN':
         new_year = year
         if year < 0:
             new_year = (year + 1) % 400
         return ce_leap_year(new_year)
-    elif type == 'CE':
+    elif cal_type == 'CE':
         return ce_leap_year(year)
-    elif type == 'JULIAN':
+    elif cal_type == 'JULIAN':
         return jul_leap_year(year)
-    elif type == 'ENGLISH':
+    elif cal_type == 'ENGLISH':
         if year >= 1800:
             return ce_leap_year(year)
-        elif year < 1800:
+        else:
             return jul_leap_year(year)
-    elif type == 'ROMAN':
+    elif cal_type == 'ROMAN':
         if year >= 1700:
             return ce_leap_year(year)
-        elif year < 1700:
+        else:
             return jul_leap_year(year)
 
 
+# Check if value is an integer value.
 def check_int(value):
     try:
         int(value)
@@ -57,7 +71,8 @@ def check_int(value):
     return 1
 
 
-def is_real_date(year, month, date, type):
+# Check if the date (year, month, date) exists if cal_type.
+def is_real_date(year, month, date, cal_type):
     month30 = (4, 6, 9, 11)
     if month not in range(1, 13):
         return 2
@@ -65,27 +80,28 @@ def is_real_date(year, month, date, type):
         return 3
     if not check_int(date) or date < 1 or date > 31:
         return 4
-    if type not in types:
+    if cal_type not in CAL_TYPES:
         return 5
-    if not year and type != 'CE':
+    if not year and cal_type != 'CE':
         return 6
     else:
         if month in month30 and date > 30:
             return 7
-        elif month == 1 and not (date < 29 or (is_leap_year(year, type)
+        elif month == 1 and not (date < 29 or (is_leap_year(year, cal_type)
                                  and date == 29)):
             return 7
-    if type == 'ENGLISH':
+    if cal_type == 'ENGLISH':
         if month == 9 and date > 2 and date < 15 and year == 1752:
             return 8
-    if type == 'ROMAN':
+    if cal_type == 'ROMAN':
         if month == 10 and date > 4 and date < 16 and year == 1582:
             return 8
     return 1
 
 
-def add_xxyy(year, type):
-    if type != 'CE':
+# Figures out value to add from last two digits of year.
+def add_xxyy(year, cal_type):
+    if cal_type != 'CE':
         if year < 0:
             new_year = (year + 1) % 100
         else:
@@ -96,32 +112,38 @@ def add_xxyy(year, type):
             int((new_year % 12) / 4)) % 7)
 
 
+# Returns value calculated from every digit of the year besides the last 2
+# digits for CE style dates.
 def ce_add_yyxx(y):
     yyxx = (2, 0, 5, 3)
     return yyxx[int(y / 100) % 4]
 
 
+# Returns value calculated from every digit of the year besides the last 2
+# digits for Julian style dates.
 def jul_add_yyxx(y):
     return (7 - int(y / 100)) % 7
 
 
-def add_yyxx(year, month, date, type):
-    if type == 'GREGORIAN':
+# Figures out value to add from every digit of the year besides the last 2
+# digits.
+def add_yyxx(year, month, date, cal_type):
+    if cal_type == 'GREGORIAN':
         new_year = year
         if year < 0:
             new_year = (year + 1) % 400
         new_year = new_year % 400
         return ce_add_yyxx(new_year)
-    elif type == 'CE':
+    elif cal_type == 'CE':
         new_year = year % 400
         return ce_add_yyxx(new_year)
-    elif type == 'JULIAN':
+    elif cal_type == 'JULIAN':
         new_year = year
         if year < 0:
             new_year = (year + 1) % 700
         new_year %= 700
         return jul_add_yyxx(new_year)
-    elif type == 'ENGLISH':
+    elif cal_type == 'ENGLISH':
         if year >= 1752:
             if year == 1752:
                 if month in range(9, 13):
@@ -143,7 +165,7 @@ def add_yyxx(year, month, date, type):
                 new_year = (year + 1) % 700
             new_year %= 700
             return jul_add_yyxx(new_year)
-    elif type == 'ROMAN':
+    elif cal_type == 'ROMAN':
         if year >= 1582:
             if year == 1582:
                 if month in range(10, 13):
@@ -167,36 +189,25 @@ def add_yyxx(year, month, date, type):
             return jul_add_yyxx(new_year)
 
 
-def add_year(year, month, date, type):
-    return add_yyxx(year, month, date, type) + add_xxyy(year, type)
+# Add value calculated from the year.
+def add_year(year, month, date, cal_type):
+    return add_yyxx(year, month, date, cal_type) + add_xxyy(year, cal_type)
 
 
-def add_month(year, month, type):
-    if is_leap_year(year, type) and len(month_offset[month - 1]) > 1:
-        return month_offset[month - 1][1]
-    return month_offset[month - 1][0]
+# Add value for the month based on the year and cal_type.
+def add_month(year, month, cal_type):
+    if is_leap_year(year, cal_type) and len(MONTH_OFFSET[month - 1]) > 1:
+        return MONTH_OFFSET[month - 1][1]
+    return MONTH_OFFSET[month - 1][0]
 
 
-def date(year, month, date, type='ENGLISH'):
-    month_name = month_names[month - 1]
-    type = type.upper()
-    check = is_real_date(year, month, date, type)
+# Returns the day of the week or raises error if a date can't be calculated.
+def date(year, month, date, cal_type='ENGLISH'):
+    cal_type = cal_type.upper()
+    check = is_real_date(year, month, date, cal_type)
     if check == 1:
-        total = ((add_year(year, month, date, type) +
-                 add_month(year, month, type) + date) % 7)
-        return days[total]
-    if type != 'CE':
-        type = type.capitalize()
-    exceptions = {
-        2: 'Expected integer between 1 and 12 for month',
-        3: 'Expected integer for year',
-        4: 'Expected integer between 1 and 31 for date',
-        5: '%s is not an available calendar type' % (type),
-        6: 'The %s Calendar does not have a year %s' % (type, str(year)),
-        7: '%s does not have %s days in it' % (month_name, str(date)),
-        8: 'The %s Calendar did not have that date' % (type)
-    }
-    if check in range(2, 9):
-        raise Exception(exceptions[check])
-
-print(add_year(2014, 3, 26, "ENGLISH"))
+        total = ((add_year(year, month, date, cal_type) +
+                 add_month(year, month, cal_type) + date) % 7)
+        return DAYS[total]
+    raise Exception("Cannot Calculate Date %s, %s, %s, %s" %
+                    (str(year), str(month), str(date), cal_type))
